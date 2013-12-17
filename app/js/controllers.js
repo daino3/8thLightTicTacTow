d645581904138fd536de347a81a581c61f710def
daino3 <dainovu3@gmail.com>
2013-12-16 18:20:42 -0600

    added firebase to code base

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,15 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+var myControllers = angular.module('myApp.controllers', [])
 
+myControllers.controller('MyCtrl1', ['$firebase', '$scope',
+  function($firebase, $scope) {
+  var ref = new Firebase("https://tictactoe-dainer.firebaseio.com/");
+    $scope.messages = $firebase(ref);
+    $scope.addMessage = function(e) {
+      if (e.keyCode != 13) return;
+      $scope.messages.$add({from: $scope.name, body: $scope.msg});
+      $scope.msg = "";
+    };
   }])
-  .controller('MyCtrl2', [function() {
-
-  }]);
