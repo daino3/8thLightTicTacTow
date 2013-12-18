53c16cec4bafea9fa67ceb7953d8433929a7e8ad
daino3 <dainovu3@gmail.com>
2013-12-17 18:29:27 -0600

    adding 'winning/gameover' logic and checks

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -14,47 +14,82 @@ myControllers.controller('Game', ['$firebase', '$scope',
     
     var ref = new Firebase("https://tictactoe-dainer.firebaseio.com/");
     
-    $scope.messages = $firebase(ref);
+    $scope.winner = false
     $scope.playerMarker = "X"
-    $scope.rows = [
-      [{'id' : 'A1','letter': '','class': 'box'},
-       {'id' : 'A2','letter': '','class': 'box'},
-       {'id' : 'A3','letter': '','class': 'box'}],
-      [{'id' : 'B1','letter': '','class': 'box'},
-       {'id' : 'B2','letter': '','class': 'box'},
-       {'id' : 'B3','letter': '','class': 'box'}],
-      [{'id' : 'C1','letter': '','class': 'box'},
-       {'id' : 'C2','letter': '','class': 'box'},
-       {'id' : 'C3','letter': '','class': 'box'}]
+    $scope.board = [
+      [{'id' : 'A1','letter': ''},
+       {'id' : 'A2','letter': ''},
+       {'id' : 'A3','letter': ''}],
+      [{'id' : 'B1','letter': ''},
+       {'id' : 'B2','letter': ''},
+       {'id' : 'B3','letter': ''}],
+      [{'id' : 'C1','letter': ''},
+       {'id' : 'C2','letter': ''},
+       {'id' : 'C3','letter': ''}]
     ];
-    
-    $scope.addMessage = function(e) {
-      if (e.keyCode != 13) return;
-      $scope.messages.$add({from: $scope.name, body: $scope.msg});
-      $scope.msg = "";
-    };
 
     $scope.takeSquare = function(column) {
-      column.letter = $scope.playerMarker
-      // var element = angular.element(column.target)
-      // element.append($currentPlayer);
-      // $scope.switchTurn();
-      $scope.switchTurn();
+      if (!column.letter) {
+        column.letter = $scope.playerMarker
+        $scope.checkForWinner();
+      }
+      else {
+        alert('select an open square!!')
+      }
     };
 
-    $scope.switchTurn = function() {
-      // console.log($scope.rows[0][0].letter = "O")
-      // console.log($scope.rows[1][0].letter = "O")
-      // console.log($scope.rows[2][0].letter = "O")
+    $scope.switchPlayer = function() {
+      if (!$scope.winner) {
+        $scope.playerMarker = ($scope.playerMarker == 'X') ? "O" : "X";
+      }
+      else {
+        alert($scope.winner + 'has won!!')
+      }
     };
 
-    $scope.playGame = function(gameObject) {
+    $scope.checkForWinner = function() {
+      $scope.checkBoard();
+      $scope.switchPlayer();
+    }
+
+    $scope.checkBoard = function(){
+      $scope.checkRows();
+      $scope.checkColumns();
+      $scope.checkDiagonals();
+    };
+
+    $scope.checkRows = function(){
+      angular.forEach($scope.board, function(row) {
+        var collection = []
+        angular.forEach(row, function(box) {
+          collection.push(box.letter)
+        })
+        if (collection.allSameValues()) { 
+          alert("We Have a Winner!!")
+          $scope.gameOver = true; 
+        };  
+      })
+    }
+
+    $scope.checkColumns = function() {
+    
+    }
+
+    $scope.checkDiagonals = function() {
 
     }
 
-    $scope.playCenter = function(rows) {
+    $scope.gameOver = function() {
 
     }
 
   }])
 
+Array.prototype.allSameValues = function() {
+  for (var i = 1; i < this.length; i++) {
+    if (this[i] !== this[0] || this[0] === "") {
+      return false; 
+    }
+  } 
+  return true;
+}
