07d07e3e49475f1d923f6023373dfb909c9f60d8
daino3 <dainovu3@gmail.com>
2013-12-20 11:30:15 -0600

    migrating logic from controller to services

--- app/js/services.js
+++ app/js/services.js
@@ -9,8 +9,33 @@ var appServices = angular.module('services', []);
 
 appServices.value('version', '0.1');
 
-appServices.factory('gameBoard', 
+appServices.factory('gameService', 
   function(){
-    return "Hello"
+    return {
+      greeting: function() {
+        return "Hello"
+      },
+
+      gameBoard: function() {
+        var board = [
+          [{'id' : 'A1','letter': ''}, {'id' : 'A2','letter': ''}, {'id' : 'A3','letter': ''}],
+          [{'id' : 'B1','letter': ''}, {'id' : 'B2','letter': ''}, {'id' : 'B3','letter': ''}],
+          [{'id' : 'C1','letter': ''}, {'id' : 'C2','letter': ''}, {'id' : 'C3','letter': ''}]
+        ]; 
+        return board 
+      },
+
+      takeSquare: function(box) {
+        if (!box.letter) {
+          box.letter = playerMarker;
+          // checkForWinner();
+        }
+        else {
+          alert('select an open square!!');
+        }
+      }
+
+
+    }
   }
 );
