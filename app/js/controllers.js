80e5dedda9e7831a686d2cc56301920e69aa1956
daino3 <dainovu3@gmail.com>
2013-12-20 11:30:15 -0600

    migrating logic from controller to services

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,15 +4,20 @@
 
 var myControllers = angular.module('controllers', ['services'])
 
-myControllers.controller('Welcome', ['$scope', 'gameBoard', 
-  function($scope, gameBoard) {
-    $scope.gameBoard
+myControllers.controller('Welcome', ['$scope', 'gameService', 
+  function($scope, gameService) {
+
+    $scope.message = "" 
+
+    $scope.sayGreeting = function() {
+      $scope.message = gameService.greeting(); // calls greeting function fom gameService
+    }
   }])
 
 // ----------- LOCAL GAME ------------------//
 
-myControllers.controller('LocalGame', ['$firebase', '$scope', 
-  function($firebase, $scope) {
+myControllers.controller('LocalGame', ['$firebase', '$scope', 'gameService',
+  function($firebase, $scope, gameService) {
     
     var ref = new Firebase("https://tictactoe-dainer.firebaseio.com/");
 
@@ -20,11 +25,7 @@ myControllers.controller('LocalGame', ['$firebase', '$scope',
     // $scope.losses = []
     $scope.winner = false
     $scope.playerMarker = "X"
-    $scope.board = [
-      [{'id' : 'A1','letter': ''}, {'id' : 'A2','letter': ''}, {'id' : 'A3','letter': ''}],
-      [{'id' : 'B1','letter': ''}, {'id' : 'B2','letter': ''}, {'id' : 'B3','letter': ''}],
-      [{'id' : 'C1','letter': ''}, {'id' : 'C2','letter': ''}, {'id' : 'C3','letter': ''}]
-    ];
+    $scope.board = gameService.gameBoard();
 
     $scope.takeSquare = function(column) {
       if (!column.letter) {
@@ -113,8 +114,8 @@ Array.prototype.allSameValues = function() {
 
 // ----------- AI GAME ------------------//
 
-myControllers.controller('AIGame', ['$firebase', '$scope',
-  function($firebase, $scope) {
+myControllers.controller('AIGame', ['$firebase', '$scope', 'gameService',
+  function($firebase, $scope, gameService) {
     
     var ref = new Firebase("https://tictactoe-dainer.firebaseio.com/");
 
@@ -123,87 +124,12 @@ myControllers.controller('AIGame', ['$firebase', '$scope',
     $scope.winner = false
     $scope.playerMarker = "X"
     $scope.compMarker = "O"
-    $scope.board = [
-      [{'id' : 'A1','letter': ''}, {'id' : 'A2','letter': ''}, {'id' : 'A3','letter': ''}],
-      [{'id' : 'B1','letter': ''}, {'id' : 'B2','letter': ''}, {'id' : 'B3','letter': ''}],
-      [{'id' : 'C1','letter': ''}, {'id' : 'C2','letter': ''}, {'id' : 'C3','letter': ''}]
-    ];
+    $scope.board = gameService.gameBoard()
 
-    $scope.takeSquare = function(column) {
-      if (!column.letter) {
-        column.letter = $scope.playerMarker
-        $scope.checkForWinner();
-      }
-      else {
-        alert('select an open square!!')
-      }
-    };
+    $scope.takeSquare = function(box) {
 
-    $scope.switchPlayer = function() {
-      if (!$scope.winner) {
-        $scope.playerMarker = ($scope.playerMarker == 'X') ? "O" : "X";
-      }
-      else {
-        alert($scope.winner + 'has won!!')
-      }
-    };
-
-    $scope.checkForWinner = function(){
-      $scope.checkRows();
-      $scope.checkColumns();
-      $scope.checkDiagonals();
-      $scope.switchPlayer();
-    };
-
-    $scope.checkRows = function(){
-      angular.forEach($scope.board, function(row) {
-        var rowContents = []
-        angular.forEach(row, function(box) {
-          rowContents.push(box.letter)
-        })
-        $scope.winCheck(rowContents)
-      })
+      // gameService.takeSquare(box)
     }
 
-    $scope.checkColumns = function() {
-      for (var i = 0; i < 3; i++) {
-        var columnContents = []
-        angular.forEach($scope.board, function(row) {
-          columnContents.push(row[i].letter);
-        })
-        $scope.winCheck(columnContents)
-      }
-    }
-
-    $scope.checkDiagonals = function() {
-      var b = $scope.board
-      var topBottom = [b[0][0].letter, b[1][1].letter, b[2][2].letter]
-      var bottomTop = [b[2][0].letter, b[1][1].letter, b[0][2].letter]
-      $scope.winCheck(topBottom)
-      $scope.winCheck(bottomTop);
-    }
-
-    $scope.resetBoard = function() {
-      angular.forEach($scope.board, function(row) {
-        angular.forEach(row, function(box) {
-          box.letter = ""
-        })
-      })
-    }
-
-    $scope.winCheck = function(group) {
-      if ( group.allSameValues() ) { 
-        alert("We Have a Winner!!")
-        // $scope.gameOver = true; 
-      };  
-    }
-
-    // $scope.gameOver = function() {
-
-    // }
-
-    $scope.aIMove = function() {
-
-    }
 
   }])
