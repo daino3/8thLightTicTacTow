fcce8dfc326c153a1b7c0c8a3bc1a2d6a7b17afc
daino3 <dainovu3@gmail.com>
2014-01-03 17:31:43 -0600

    added AI logic for blocks and winning the game

--- app/js/services.js
+++ app/js/services.js
@@ -46,32 +46,88 @@ appServices.factory('gameService',
           angular.forEach(row, function(box) {
             rowContents.push(box.letter)
           })
-          if (rowContents.allSameValues() === true) {
+          if (rowContents.allSameValues()) {
             foundWinner = true;
           }
         })
-        return foundWinner
+        return foundWinner;
       },
 
       checkColumns: function(board) {
+        var foundWinner = false
         for (var i = 0; i < 3; i++) {
           var columnContents = []
           angular.forEach(board, function(row) {
             columnContents.push(row[i].letter);
           })
-          if (columnContents.allSameValues()) return true;
+          if (columnContents.allSameValues()) {
+            foundWinner = true;
+          }
         }
+        return foundWinner;
       },
 
       checkDiagonals: function(board) {
+        var foundWinner = false
         var topBottom = [board[0][0].letter, board[1][1].letter, board[2][2].letter]
         var bottomTop = [board[2][0].letter, board[1][1].letter, board[0][2].letter]
-        if (topBottom.allSameValues() || bottomTop.allSameValues()) return true;
+        if (topBottom.allSameValues() || bottomTop.allSameValues()) {
+          foundWinner = true;
+        }
+        return foundWinner
       },
 
       flipCoin: function() {
         var coin = Math.floor(Math.random() * 2);
         return (coin === 0) ? "O" : "X";
+      },
+
+      //--------------- AI logic ----------------//
+
+      blockorWinRow: function(board, compMarker) {
+        for (var row = 0; row < board.length; row++) {
+          var rowContents = []
+          for (var index = 0; index < board.length; index++) {
+            rowContents.push(board[row][index].letter)
+          }
+          if (rowContents.hasTwoSameValues("X", "O") && rowContents.hasEmptyBox()) {
+            var box = rowContents.indexOf("");
+            board[row][box].letter = compMarker
+          }
+        }
+        return board
+      },
+
+      blockorWinColumn: function(board, compMarker) {
+        for (var col = 0; col < board.length; col++) {
+          var columnContents = []
+          for (var index = 0; index < board.length; index++) {
+            columnContents.push(board[index][col].letter)
+          }
+          if (columnContents.hasTwoSameValues("X", "O") && columnContents.hasEmptyBox()) {
+            var box = columnContents.indexOf("");
+            board[box][col].letter = compMarker
+          }
+        }
+        return board
+      },
+
+      blockorWinDiagonal: function(board, compMarker) {
+        var topBottom = [board[0][0].letter, board[1][1].letter, board[2][2].letter]
+        var bottomTop = [board[2][0].letter, board[1][1].letter, board[0][2].letter]
+        if (topBottom.hasTwoSameValues("X", "O") && topBottom.hasEmptyBox()) {
+          var index = topBottom.indexOf("");
+          board[index][index].letter = compMarker
+          return board 
+        }
+        else if (bottomTop.hasTwoSameValues("X", "O") && bottomTop.hasEmptyBox()){
+          var box = bottomTop.indexOf("");
+          board[2 - box][box].letter = compMarker // middle will never be open due to AI logic - see 'takemiddleifopen()'
+          return board
+        }
+        else {
+          return board
+        }
       },
 
     }
@@ -87,4 +143,22 @@ Array.prototype.allSameValues = function() {
     }
   } 
   return true;
+}
+
+Array.prototype.hasTwoSameValues = function(val1, val2) {
+  var val1counter = 0
+  var val2counter = 0
+  for (var i = 0; i < this.length; i++) {
+    if (this[i] === val1) {val1counter += 1}
+    if (this[i] === val2) {val2counter += 1}
+  }
+  return (val1counter === 2 || val2counter === 2) ? true : false
+}
+
+Array.prototype.hasEmptyBox = function() {
+  var empty = false
+  for(var i = 0; i < this.length; i++) {
+    if (this[i] === "") {empty = true}
+  }
+  return empty
 }
